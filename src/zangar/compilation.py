cf872a8497586f4cfe0a77a84fbc4cd5c9310c62
Lee <294622946@qq.com>
2025-01-26 19:49:03 +0800

    Supports compiling schema to OpenAPI Schema Object

---
+++ src/zangar/compilation.py
@@ -0,0 +1,84 @@
+from __future__ import annotations
+
+from datetime import datetime
+from typing import Generator
+
+from zangar._core import Schema
+
+
+class OpenAPI30Compiler:
+    def compile(self, schema):
+        return self._compile(schema)
+
+    # pylint: disable=too-many-branches
+    def _compile(self, schema: Schema, parent: dict | None = None):
+        rv: dict = {}
+        for meta in _iterate_meta(schema):
+            if "type" in meta:
+                if meta["type"] is type(None):
+                    if isinstance(parent, dict):
+                        parent["nullable"] = True
+                    else:
+                        rv.update(
+                            enum=[None],
+                        )
+                else:
+                    if meta["type"] is datetime:
+                        rv.update(
+                            type="string",
+                            format="date-time",
+                        )
+                    elif meta["type"] is object:
+                        rv.update(nullable=True)  # any type
+                    else:
+                        rv["type"] = {
+                            str: "string",
+                            int: "integer",
+                            float: "number",
+                            bool: "boolean",
+                        }[meta["type"]]
+            if "item" in meta:
+                rv.update(
+                    type="array",
+                    items=self._compile(meta["item"]),
+                )
+            if "fields" in meta:
+                properties = {}
+                required = []
+                for name, field in meta["fields"].items():
+                    key = name if field._alias is None else field._alias
+                    properties[key] = self._compile(field._schema, rv)
+                    if field._required:
+                        required.append(key)
+                rv.update(type="object")
+                if properties:
+                    rv.update(properties=properties)
+                if required:
+                    rv.update(required=required)
+            if "union" in meta:
+                results = list(
+                    filter(lambda i: i, [self._compile(s, rv) for s in meta["union"]])
+                )
+                if len(results) > 1:
+                    rv.update(anyOf=results)
+                elif len(results) == 1:
+                    rv.update(results[0])
+            if "min" in meta:
+                rv.update(minLength=meta["min"])
+            if "max" in meta:
+                rv.update(maxLength=meta["max"])
+            if "gt" in meta:
+                rv.update(minimum=meta["gt"], exclusiveMinimum=True)
+            if "gte" in meta:
+                rv.update(minimum=meta["gte"])
+            if "lt" in meta:
+                rv.update(maximum=meta["lt"], exclusiveMaximum=True)
+            if "lte" in meta:
+                rv.update(maximum=meta["lte"])
+        return rv
+
+
+def _iterate_meta(schema: Schema) -> Generator[dict, None, None]:
+    for n in schema._iterate_chain():
+        if n._meta:
+            yield n._meta
---
+++ tests/openapi/test_openapi.py
@@ -0,0 +1,153 @@
+import json
+import os
+
+import jsonschema
+import pytest
+
+import zangar as z
+from zangar.compilation import OpenAPI30Compiler
+
+
+class TestOpenAPI30:
+    with open(os.path.join(os.path.dirname(__file__), "2024-10-18")) as fp:
+        OPENAPISPEC_30 = json.load(fp)
+
+    @classmethod
+    def compile(cls, schema):
+        schema_object = OpenAPI30Compiler().compile(schema)
+        jsonschema.validate(
+            {
+                "openapi": "3.0.3",
+                "info": {
+                    "title": "Test",
+                    "version": "1.0.0",
+                },
+                "paths": {},
+                "components": {
+                    "schemas": {
+                        "Test": schema_object,
+                    }
+                },
+            },
+            cls.OPENAPISPEC_30,
+        )
+        return schema_object
+
+    def test_nullable(self):
+        assert self.compile(z.int() | z.none()) == {
+            "type": "integer",
+            "nullable": True,
+        }
+
+    def test_datetime(self):
+        assert self.compile(z.datetime()) == {
+            "type": "string",
+            "format": "date-time",
+        }
+
+    def test_object_type(self):
+        assert self.compile(
+            z.object(
+                {
+                    "name": z.str(),
+                }
+            )
+        ) == {
+            "type": "object",
+            "properties": {
+                "name": {
+                    "type": "string",
+                }
+            },
+            "required": ["name"],
+        }
+
+    def test_object_property_required_and_alias(self):
+        assert self.compile(
+            z.object(
+                {
+                    "name": z.field(z.str(), alias="name_alias").optional(),
+                },
+            )
+        ) == {
+            "type": "object",
+            "properties": {
+                "name_alias": {
+                    "type": "string",
+                }
+            },
+        }
+
+    def test_union(self):
+        assert self.compile(z.str() | z.int()) == {
+            "anyOf": [
+                {
+                    "type": "string",
+                },
+                {
+                    "type": "integer",
+                },
+            ],
+        }
+        assert self.compile(z.str() | z.int() | z.none()) == {
+            "anyOf": [
+                {
+                    "type": "string",
+                },
+                {
+                    "type": "integer",
+                },
+            ],
+            "nullable": True,
+        }
+
+    def test_min_max_length(self):
+        assert self.compile(z.str().min(1).max(10)) == {
+            "type": "string",
+            "minLength": 1,
+            "maxLength": 10,
+        }
+
+    def test_minim_maxim(self):
+        assert self.compile(z.int().gte(1).lte(10)) == {
+            "type": "integer",
+            "minimum": 1,
+            "maximum": 10,
+        }
+        assert self.compile(z.int().gt(1).lt(10)) == {
+            "type": "integer",
+            "minimum": 1,
+            "exclusiveMinimum": True,
+            "maximum": 10,
+            "exclusiveMaximum": True,
+        }
+
+    def test_any_type(self):
+        assert self.compile(z.any()) == {
+            "nullable": True,
+        }
+
+    def test_to_type(self):
+        assert self.compile(z.to.int()) == {
+            "type": "integer",
+        }
+
+    def test_raw_schema(self):
+        assert self.compile(z.transform(int)) == {}
+
+    def test_array(self):
+        assert self.compile(z.list()) == {
+            "type": "array",
+            "items": {
+                "nullable": True,
+            },
+        }
+        assert self.compile(z.list(z.int())) == {
+            "type": "array",
+            "items": {
+                "type": "integer",
+            },
+        }
+
+    def test_none(self):
+        assert self.compile(z.none()) == {"enum": [None]}
